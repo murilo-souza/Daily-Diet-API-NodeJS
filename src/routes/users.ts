0ebaf1d029910669c77982df786c8a1dd9f54be5
Murilo Leme de Souza <murilo.l.souza232@gmail.com>
2024-01-03 14:51:14 -0300

    Initial commit

---
+++ src/routes/users.ts
@@ -0,0 +1,41 @@
+import { FastifyInstance } from 'fastify'
+import { knex } from '../database'
+import { z } from 'zod'
+import { randomUUID } from 'node:crypto'
+
+export async function userRoutes(app: FastifyInstance) {
+  app.post('/', async (request, reply) => {
+    const createUserBodySchema = z.object({
+      name: z.string(),
+      email: z.string().email(),
+    })
+
+    const { name, email } = createUserBodySchema.parse(request.body)
+
+    let sessionId = request.cookies.sessionId
+
+    if (!sessionId) {
+      sessionId = randomUUID()
+
+      reply.setCookie('sessionId', sessionId, {
+        path: '/',
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+      })
+    }
+
+    const userByEmail = await knex('users').where({ email }).first()
+
+    if (userByEmail) {
+      return reply.status(400).send({ message: 'User already exists' })
+    }
+
+    await knex('users').insert({
+      id: randomUUID(),
+      name,
+      email,
+      session_id: sessionId,
+    })
+
+    return reply.status(201).send()
+  })
+}
